9888ba3d19cf9a73c14cf17ed53f77a9023faabe
callor <callor@callor.com>
2017-09-13 15:46:36 +0900

    2017-09-13

--- app/src/main/java/com/callor/naverbook/MainActivity.java
+++ app/src/main/java/com/callor/naverbook/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,6 +14,18 @@ import android.view.MenuItem;
 import com.callor.naverbook.databinding.ActivityMainBinding;
 import com.callor.naverbook.databinding.ContentMainBinding;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -27,8 +40,8 @@ public class MainActivity extends AppCompatActivity {
 
         mainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
         contentBinding = mainBinding.contentMain;
-
         setSupportActionBar(mainBinding.toolbar);
+
 
         mainBinding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,6 +50,71 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    public void naverSearch() {
+
+        try {
+
+            String text = URLEncoder.encode("JAVA","UTF-8");
+            String apiURL = "https://openapi.naver.com/v1/search/book.json";
+            apiURL += "?query=" + contentBinding.txtBookSearch.getText().toString();
+            apiURL += "&display=20" ;
+
+            URL url = new URL(apiURL);
+            HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+
+            // Header 만들기
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("X-Naver-Client-Id",NaverSecret.NAVER_CLIENT_ID);
+            connection.setRequestProperty("X-Naver-Client-Secret",NaverSecret.NAVER_CLIENT_SECRET);
+
+            // 네이버에 GET으로 조회하는 주체
+            int responseCode = connection.getResponseCode();
+            BufferedReader buffer ;
+            if(responseCode == 200) { // 요청이 정상적이어서 데이터가 정상으로 도착할 것이다.
+                buffer = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            } else {
+                return ;
+            }
+            String inputLine ;
+            String jsonString = new String();
+
+            // 데이터가 정상적으로 읽고 있으면
+            while((inputLine = buffer.readLine()) != null) {
+                jsonString += inputLine;
+            }
+            buffer.close(); //  네트워크 종료
+
+            // 네이버로 부터 전송되는 data는 json 구조를 가진 String이므로
+            // json Object로 변경해줘야 내부에서 원활하게 사용할 수 있다.
+            JSONObject resJSON = new JSONObject(jsonString);
+
+            // 도서목록(정보가 담겨있는)
+            //      네이버는 items라는 이름으로 Array형태의 데이터를 보낸다
+            //          items key를 참조하여 하위 Data를 JSONArray 형태로 추출한다.
+            JSONArray items = resJSON.getJSONArray("items");
+            for(int i = 0 ; i < items.length() ; i++) {
+
+                // item을 하나씩 뽑아내기
+                JSONObject item = items.getJSONObject(i);
+                Log.d("NAVER:",item.getString("title")); // 디버깅창에서 데이터 확인하기
+
+            }
+
+        } catch (UnsupportedEncodingException e) {
+            // URLEncoder
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            // URL 생성할때
+            e.printStackTrace();
+        } catch (IOException e) {
+            // 읽을때
+            e.printStackTrace();
+        } catch (JSONException e) {
+            // JSONObject로 변경할때
+            e.printStackTrace();
+        }
     }
 
     @Override
--- app/src/main/java/com/callor/naverbook/NaverSecret_sample.java
+++ app/src/main/java/com/callor/naverbook/NaverSecret_sample.java
@@ -6,7 +6,7 @@ package com.callor.naverbook;
  *  NAVER_CLIENT_ID와 NAVER_CLIENT_SECRET 항목을
  *  본인의 NAVER 개발 APP의 정보로 수정한 후
  *  파일이름을 NaverSecret로 변경한 후 사용하세요
- *  
+ *
  */
 
 public class NaverSecret_sample {
